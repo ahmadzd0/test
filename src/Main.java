1beb51db7eac23e2c093e2a57a0de503edd608a5
AhmadD <azdaghmash@gmail.com>
2024-09-02 12:00:09 +0300

    Print User Result.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       int x=4;
+       int y=5;
+       int result=x+y;
+       String name=" sameeh";
+       String name2="ahmad";
+        System.out.println("Hello and welcome!");
+        System.out.println(5<5);
+        System.out.println("x+y="+ result);
+        System.out.println(name+name2);
+
+
+
+
+    }
+}
